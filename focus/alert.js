91e34d039b26db64d263e59b8b7207fb1c4e3f37
Lukáš Mužík <lukasmuzik.dev@gmail.com>
2025-11-23 16:05:41 +0100

    feat: add mvp blocking for certain sites

--- focus/alert.js
+++ focus/alert.js
@@ -12,3 +12,55 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.tabs.create({ url: "finished.html" });
   }
 });
+
+// --- Blocking Logic ---
+
+const BLOCKED_SITES = [
+  "youtube.com",
+  "youtu.be",
+  "facebook.com",
+  "instagram.com",
+  "twitter.com",
+  "x.com",
+  "reddit.com",
+];
+
+function setBlocking(enabled) {
+  chrome.declarativeNetRequest.getDynamicRules((previousRules) => {
+    const previousRuleIds = previousRules.map((rule) => rule.id);
+
+    const rules = enabled
+      ? BLOCKED_SITES.map((domain, index) => ({
+          id: index + 1,
+          priority: 1,
+          action: { type: "block" },
+          condition: {
+            urlFilter: `||${domain}`,
+            resourceTypes: [
+              "main_frame",
+              "sub_frame",
+              "xmlhttprequest",
+              "script",
+            ],
+          },
+        }))
+      : [];
+
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: previousRuleIds,
+      addRules: rules,
+    });
+  });
+}
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (namespace === "local" && changes.targetTime) {
+    if (changes.targetTime.newValue) {
+      console.log("Blocking enabled");
+      setBlocking(true);
+    } else {
+      console.log("Blocking disabled");
+      setBlocking(false);
+    }
+  }
+});
